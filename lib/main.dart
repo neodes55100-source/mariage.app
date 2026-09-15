b30fdaa8368e94c2f28c910dbf0f31f68dd4ec7e
neodes55100-source <neodes55100@gmail.com>
2026-09-15 04:41:17 +0200

    Active le moteur de tâches en arrière-plan sur iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,16 @@
+import 'dart:io';
+
+import 'package:flutter/widgets.dart';
+import 'package:workmanager/workmanager.dart';
+
 import 'wedding_app_site.dart' as wedding_app;
 
-Future<void> main() => wedding_app.main();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isIOS) {
+    await Workmanager().initialize(wedding_app.callbackDispatcher);
+  }
+
+  await wedding_app.main();
+}
